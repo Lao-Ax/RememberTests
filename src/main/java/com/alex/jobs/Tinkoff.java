182304be962ab2690d28f764924ae610e0c4d790
aplekhov <aplekhov@wiley.com>
2022-12-25 12:48:47 +0300

    Add comment

--- src/main/java/com/alex/jobs/Tinkoff.java
+++ src/main/java/com/alex/jobs/Tinkoff.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Tinkoff {
 
+    // Найти недостающее число из набора от 0 до N.
     public static void main(String[] args) {
         int[] arr = {5, 3, 2, 1, 0};
         int N = arr.length + 1;
